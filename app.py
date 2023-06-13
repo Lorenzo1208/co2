1b3bd579a237970663d962ccc6ee62a98b00c03f
Lorenzo G <55632146+Lorenzo1208@users.noreply.github.com>
2023-06-13 15:36:53 +0200

    update

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, Table, select
 
 app = Flask(__name__)
-
-user = os.getenv('DB_USER')
-password = os.getenv('DB_PASSWORD')
-host = os.getenv('DB_HOST')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:5432/co2'
+database_uri = os.getenv('DATABASE_URI')
+if not database_uri:
+    raise RuntimeError("DATABASE_URI not set in environment")
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+print(f"Database URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
 
 db = SQLAlchemy(app)
+meta = MetaData()
 
-class Co2(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # ajoutez d'autres attributs selon la structure de votre table 'co2'
+Co2 = None  # Declare Co2 here
+
+@app.before_request
+def load_table():
+    global Co2  # Use the global Co2 variable
+    if Co2 is None:
+        print("Loading table 'co2'...")
+        # Reflect your existing database into a new model
+        Co2 = Table('co2', meta, autoload_with=db.engine)
+        print("Table 'co2' loaded successfully.")
 
 @app.route('/')
 def index():
-    data = Co2.query.all()
+    print("Processing request for index...")
+    stmt = select(Co2)  # construct the SELECT * FROM co2 query
+    with db.engine.begin() as connection:
+        result = connection.execute(stmt)  # execute the query
+        data = result.fetchall()  # fetch all rows from the result
     return render_template('index.html', data=data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    print("Starting the app...")
+    app.run(debug=True, port=8000)  # Run the app on port 8000
