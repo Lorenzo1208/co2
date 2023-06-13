a6417d277de9614ac859b2fdd7f1de24ed2f747b
Lorenzo G <55632146+Lorenzo1208@users.noreply.github.com>
2023-06-13 11:15:26 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+port = os.getenv('DB_PORT')
+database = os.getenv('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+
+db = SQLAlchemy(app)
+
+class Co2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # ajoutez d'autres attributs selon la structure de votre table 'co2'
+
+@app.route('/')
+def index():
+    data = Co2.query.all()
+    return render_template('index.html', data=data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
